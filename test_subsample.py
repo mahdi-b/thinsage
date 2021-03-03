4d6ff4a868af9e56707d6d358e525cbb21498b1c
Kameron Wong <wongk711@hawaii.edu>
2021-03-03 00:59:07 -1000

    finished imbalanced stratified with pytest

--- test_subsample.py
+++ test_subsample.py
@@ -117,7 +117,7 @@ def test_stratified_balanced_subsample():
             assert np.abs(lengths[key1] - lengths[key2]) <= 1
 
 
-def test_stratified_balanced_subsample_obj():
+def test_stratified_subsample_balanced_obj():
     # initialize data
     data_size = 100
     classes = ['red', 'blue', 'green']
@@ -125,8 +125,66 @@ def test_stratified_balanced_subsample_obj():
     test_list = []
     class_list = []
     for i in range(data_size):
-        test_list.append(DummyObj(classId=classes[i % num_classes]))
-        class_list.append(classes[i % num_classes])
+        this_class = np.random.choice(classes)
+        test_list.append(DummyObj(classId=this_class))
+        class_list.append(this_class)
+    # sub sample from data
+    test_size = 45
+    test_sub_sample, sub_class_list = stratified_subsample_balanced(test_list, class_list, test_size)
+    # check returned samples is equal to specified size
+    assert len(test_sub_sample) == test_size
+    # check that type of items returned is same as when it went in
+    if type(test_list[0]) is list:
+        assert type(test_list[0][0]) is type(test_sub_sample[0][0])
+    else:
+        assert type(test_list[0]) is type(test_sub_sample[0])
+
+    for i in range(test_size):
+        # check all that are returned are from the original set
+        assert test_sub_sample[i] in test_list
+        # check returned class is same as the original
+        assert test_sub_sample[i].classID == sub_class_list[i]
+        # check no duplicates were returned
+        for j in range(i+1, test_size):
+            assert test_sub_sample[i].UUID != test_sub_sample[j].UUID
+
+    # check first that number of unique classes in returned data are same as
+    # original number of classes
+    assert len(np.unique(sub_class_list)) == num_classes
+    # check proportions of data from each class are close to balanced
+    # (within) err of +/- 1
+    lengths = {}
+    for i in range(len(sub_class_list)):
+        if sub_class_list[i] not in lengths.keys():
+            lengths[sub_class_list[i]] = 1
+        else:
+            lengths[sub_class_list[i]] += 1
+    for key1 in lengths.keys():
+        for key2 in lengths.keys():
+            if key1 == key2:
+                continue
+            assert np.abs(lengths[key1] - lengths[key2]) <= 1
+
+
+def test_stratified_subsample_imbalanced_obj():
+    # initialize data
+    data_size = 100
+    classes = ['red', 'blue', 'green', 'yellow']
+    num_classes = len(classes)
+    test_list = []
+    class_list = []
+    proportions = {}
+    for this_class in classes:
+        proportions[this_class] = 0
+    for i in range(data_size):
+        this_class = np.random.choice(classes)
+        test_list.append(DummyObj(classId=this_class))
+        class_list.append(this_class)
+        proportions[this_class] += 1
+
+    for this_class in classes:
+        proportions[this_class] = float(proportions[this_class] / data_size)
+
     # sub sample from data
     test_size = 25
     test_sub_sample, sub_class_list = stratified_subsample_balanced(test_list, class_list, test_size)
@@ -147,19 +205,20 @@ def test_stratified_balanced_subsample_obj():
         for j in range(i+1, test_size):
             assert test_sub_sample[i].UUID != test_sub_sample[j].UUID
 
-    # check first that number of uncique classes in returned data are same as
+    # check first that number of unique classes in returned data are same as
     # original number of classes
     assert len(np.unique(sub_class_list)) == num_classes
-    # check proportions of data from each class are close to balanced
-    # (within) err of +/- 1
+    # check proportions of data from each class are close to original proportions
+    # (within) err of +/- num_classes
+    # get number of items per class
     lengths = {}
     for i in range(len(sub_class_list)):
         if sub_class_list[i] not in lengths.keys():
             lengths[sub_class_list[i]] = 1
         else:
             lengths[sub_class_list[i]] += 1
-    for key1 in lengths.keys():
-        for key2 in lengths.keys():
-            if key1 == key2:
-                continue
-            assert np.abs(lengths[key1] - lengths[key2]) <= 1
+    # check against original proportions
+    for key in lengths.keys():
+        diff = np.abs(lengths[key] - (proportions[key] * test_size))
+        # best accuracy achievable?
+        assert diff <= num_classes
--- thinsage_subsample.py
+++ thinsage_subsample.py
@@ -53,6 +53,24 @@ def random_subsample(data, size=0.25, axis=0):
     return ret_list
 
 
+def stratified_subsample(data, class_list, size, balanced=True):
+    """
+    maybe use this for user ease of access
+    :param data: list/collection of type Object. Contains the samples to be sub-sampled from.
+    :param class_list: list of class identifiers, classList[i] = x implies data[i] belongs to class x
+                       where x can be of type int, string, or object
+    :param size: Integer to determine how many samples desired, or float between 0 and 1 to represent
+                 percentage of samples to be taken.
+    :param balanced: boolean, If True, original proportions of class data is ignored and even proportions
+                     will be returned (50:50 for 2 class data)
+                     If false, original proportions will be kept. Prioritize proportions over desired size
+    :return: randomized list containing balanced number samples from each class in the given data set
+             matching class_list to indicate classes of the sub-samples.
+    """
+    return stratified_subsample_balanced(data, class_list, size) if balanced else \
+        stratified_subsample_imbalanced(data, class_list, size)
+
+
 def stratified_subsample_balanced(data, class_list, size):
     """
     TO-DO: check for numpy array data type
@@ -95,13 +113,13 @@ def stratified_subsample_balanced(data, class_list, size):
     # while keeping track of the assigned classes
     combined_list = []
     sub_class_list = []
-    for i in range(num_classes):
+    for i, key in enumerate(class_data.keys()):
         if i == (num_classes - 1):
             num_per_class = num_samples
         else:
             num_per_class = int(round(num_samples / (num_classes-i)))
             num_samples -= num_per_class
-        random_perm = np.random.permutation(class_data[class_list[i]])
+        random_perm = np.random.permutation(class_data[key])
         for j in range(num_per_class):
             combined_list.append(data[random_perm[j]])
             sub_class_list.append(class_list[random_perm[j]])
@@ -115,15 +133,73 @@ def stratified_subsample_balanced(data, class_list, size):
         class_final.append(sub_class_list[final_perm[i]])
     return combined_final, class_final
 
-# def stratified_subsample_imbalanced(data, class_list, size):
-    # num_classes = np.unique(class_list)
-    #
-    # if isinstance(data, list):
-    #     data_size = len(data)
-    # else:
-    #     data_size = data.shape[0]
-    #
-    # num_samples = check_size_parameter(size, data_size)
-    # if num_samples == -1:
-    #     raise Exception(f"Invalid value was given for parameter \'size\'")
 
+def stratified_subsample_imbalanced(data, class_list, size):
+
+    num_classes = len(np.unique(class_list))
+
+    # check validity of data and get data size
+    if isinstance(data, list):
+        data_size = len(data)
+    else:
+        data_size = data.shape[0]
+
+    if data_size != len(class_list):
+        raise Exception(f'')
+
+    # check size parameter and get number of samples desired
+    num_samples = check_size_parameter(size, data_size)
+    if num_samples == -1:
+        raise Exception(f"Invalid value was given for parameter \'size\'")
+
+    # calculate original proportions of classes
+    proportions = {}
+    for this_class in np.unique(class_list):
+        proportions[this_class] = 0
+
+    for this_class in class_list:
+        proportions[this_class] += 1
+
+    for this_class in np.unique(class_list):
+        # proportions[this_class] = round(float(proportions[this_class] / data_size) * num_samples)
+        proportions[this_class] = float(proportions[this_class] / data_size)
+
+    # alter proportions to instead hold number of samples per class desired
+    # (convert float to whole numbers)
+    count = 0
+    for i, key in enumerate(proportions.keys()):
+        proportions[key] = round(proportions[key] * num_samples)
+        if i == num_classes - 1:
+            proportions[key] = num_samples - count
+        else:
+            add = round(proportions[key] * num_samples)
+            proportions[key] = add
+            count += add
+
+    # make dictionary key is class and value is list of indices in data
+    # where item is of class key
+    class_data = {}
+    for i in range(data_size):
+        if class_list[i] not in class_data.keys():
+            class_data[class_list[i]] = [i]
+        else:
+            class_data[class_list[i]].append(i)
+
+    # take samples equal to proportions from each class and combine into one list
+    # while keeping track of the assigned classes
+    combined_list = []
+    sub_class_list = []
+    for key in class_data.keys():
+        random_perm = np.random.permutation(class_data[key])
+        for i in range(proportions[key]):
+            combined_list.append(data[random_perm[i]])
+            sub_class_list.append(class_list[random_perm[i]])
+
+    # shuffle the combined list so that the returned list is NOT sorted by class
+    final_perm = np.random.permutation(len(combined_list))
+    combined_final = []
+    class_final = []
+    for i in range(len(final_perm)):
+        combined_final.append(combined_list[final_perm[i]])
+        class_final.append(sub_class_list[final_perm[i]])
+    return combined_final, class_final
